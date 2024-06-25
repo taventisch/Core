fc31b2f478b1cdec4c1d27dbd8036a0d24caa662
DevTaventix <thesonecoping@gmail.com>
2024-06-25 14:20:52 +0200

    test

--- src/main/java/de/dev/taventix/Core.java
+++ src/main/java/de/dev/taventix/Core.java
@@ -5,7 +5,7 @@ import de.dev.taventix.events.WelcomeEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
-
+d
     @Override
     public void onEnable() {
         getCommand("Info").setExecutor(new Info());
